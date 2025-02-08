e690d7d39386ff0ded29ea5d4b6047def9713a6d
Jacob-Hahn3 <jacob.hahn2024@gmail.com>
2025-02-08 16:10:55 -0500

    trying to put image selected on hub.html

--- website/scripts/modalscript.js
+++ website/scripts/modalscript.js
@@ -28,3 +28,26 @@ window.addEventListener("click", (event) => {
         }
     });
 });
+
+//retrieve selected image from localStorage
+window.onload = function() {
+    // Retrieve selected image from localStorage
+    let selectedImage = localStorage.getItem('selectedImage');
+
+    // Find the image container
+    let container = document.getElementById('imageContainer');
+
+    if (selectedImage) {
+        // Create an img element
+        let img = document.createElement('img');
+        img.src = selectedImage; // Use the saved image path
+        img.alt = "Selected Image";
+        img.className = "selected-image";
+
+        // Add image to the container
+        container.appendChild(img);
+    } else {
+        // Show message if no image was selected
+        container.innerHTML = "<p class='message'>No image selected.</p>";
+    }
+};
